fb0c67e477780634fde6ad2e18ffc4325d75f5fe
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-08-13 18:31:31 -0500

    Adding dropdowns for animal and breed, mapping each item of the arrays to an option in the dropdown.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
+const ANIMALS = ['Dog', 'Cat', 'Bird', 'Rabbit', 'Turtle', 'Snake'];
+const breeds = [];
+
 function SearchParams() {
   const [location, setLocation] = useState('');
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+
   return (
     <div className="search-params">
       <form>
@@ -17,6 +23,35 @@ function SearchParams() {
             placeholder="Type your location here"
           />
         </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
